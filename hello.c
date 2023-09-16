25c219752a70084ad0f320a1216d92a7c7faff2f
samadhan26 <samadhanmore2914@gmail.com>
2023-09-16 18:15:20 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,7 @@
+// start with 1st program in c languages
+#include <stdio.h> // header line 
+int main() {
+   
+   printf("Hello, World!"); // printing statement 
+   return 0; // if you add int in main fuction so u have  write return 0 ; if u write void main so dont need any  wriite return 0
+}
